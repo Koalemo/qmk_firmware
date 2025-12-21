39c088c34ffbe55194610c824a6337827b9036cf
ng <noam.gallmann@gmail.com>
2025-12-21 22:55:10 +0100

    Bughack: i2c_reset not defined

--- keyboards/zsa/moonlander/matrix.c
+++ keyboards/zsa/moonlander/matrix.c
@@ -23,15 +23,17 @@
 // TODO: move this to a zsa community module once we adopt qmk25 100%.
 #include "i2c_master.h"
 
+#endif
+
 #ifndef I2C_DRIVER
 #    define I2C_DRIVER I2CD1
 #endif
+
 void i2c_reset(void) {
     i2cStop(&I2C_DRIVER);
     chThdSleepMilliseconds(10);
     i2c_init();
 }
-#endif
 
 #pragma GCC push_options
 #pragma GCC optimize("-O3")
